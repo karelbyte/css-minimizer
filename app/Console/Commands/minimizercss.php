95692b23313a837544c4bc05c3565b3869040fa8
Simon Montoya <simonmontoya19@gmail.com>
2022-08-08 15:00:51 -0500

    create new classes and ids

--- app/Console/Commands/minimizercss.php
+++ app/Console/Commands/minimizercss.php
@@ -36,26 +36,78 @@ class minimizercss extends Command
 
          preg_match_all('/class="\s*(.*?)\s*"/s', $html, $obtained_classes, PREG_SET_ORDER, 0);
         preg_match_all('/id="\s*(.*?)\s*"/s', $html, $obtained_ids, PREG_SET_ORDER, 0);
-        $classes = $obtained_classes;
-        $ids = $obtained_ids;
-        foreach ($classes as $index =>$class) {
-            $class = $class[1];
-            $classes[$index]['new_name'] = substr($class, 0, 1);
-        }
-        foreach ($ids as $index =>$id) {
-            $id = $id[1];
-            $ids[$index]['new_name'] = substr($id, 0, 1);
-        }
+        $classes = [];
+        $ids = [];
 
-        foreach ($classes as $index =>$class) {
-            $new_name = $this->check_name_exisist( $classes[$index]['new_name'], $classes);
-            $classes[$index]['new_name'] = $new_name;
-            $original_element =  $html_string->find('.' . $classes[$index][1])[0];
-            if($original_element){
-                $original_element->setAttribute('class', $classes[$index]['new_name']);
+        foreach ( $obtained_classes as $index =>$class) {
+            if (strpos($class[1]," ")) {
+                $class_array = explode(" ", $class[1]);
+                foreach ($class_array as  $item) {
+                    //echo 'class'.$item.'<br>';;
+                    $class = [
+                        0 => 'class='.$obtained_classes[$index][1],
+                        1 => $item,
+                        'original'  =>  'class='.$obtained_classes[$index][1]
+                    ];
+                    $class_name = $item;
+                    $class['new_name'] = substr($class_name, 0, 1);
+                    array_push($classes, $class);
+                }
+
+            }
+            else{
+                $class_name = $class[1];
+                $class['original'] = $class[1];
+                $class['new_name'] = substr($class_name, 0, 1);
+
+                array_push($classes, $class);
             }
 
         }
+        //dd($classes[0],$classes[1],$obtained_classes[0]);
+
+        foreach ($obtained_ids as $index =>$id) {
+            if (strpos($id[1]," ")) {
+                $id_array = explode(" ", $id[1]);
+                foreach ($id_array as $index => $item) {
+                    $id = [
+                        0 => 'id='.$item,
+                        1 => $item,
+                    ];
+                    $id_name = $item;
+                    $id['new_name'] = substr($id_name, 0, 1);
+                    array_push($ids, $id);
+                }
+            }
+            else{
+                $id_name = $id[1];
+                $id['new_name'] = substr($id_name, 0, 1);
+                array_push($ids, $id);
+            }
+           /* $id = $id[1];
+            $ids[$index]['new_name'] = substr($id, 0, 1);*/
+        }
+
+        foreach ($classes as $index =>$class) {
+
+            $new_name = $this->check_name_and_id_exisist( $classes[$index]['new_name'], $classes);
+            $classes[$index]['new_name'] = $new_name;
+            $class_new_name = $classes[$index]['new_name'];
+            if ($index > 0 && $classes[$index][0] === $classes[$index]['original']) {
+                $classes[$index]['new_name'] = $classes[$index - 1]['new_name'] . ' ' . $class_new_name;
+                $original_element =  $html_string->find('.' . $classes[$index][1])[0];
+                if($original_element){
+                    $original_element->setAttribute('class', $class_new_name);
+                }
+            }else{
+                $original_element =  $html_string->find('.' . $classes[$index][1])[0];
+                if($original_element){
+                    $original_element->setAttribute('class', $class_new_name);
+                }
+            }
+
+        }
+        dd($classes[0],$classes[1] ,$dom->outerHtml);
 
         foreach ($ids as $index =>$id) {
             $new_name = $this->check_name_exisist( $ids[$index]['new_name'], $ids);
@@ -65,28 +117,26 @@ class minimizercss extends Command
                 $original_element->setAttribute('id', $ids[$index]['new_name']);
             }
         }
-
-
+        dump($dom->outerHtml);
+        dd(count($classes),count($obtained_classes),count($ids),count($obtained_ids));
         $this->css_to_minimizer($html, $ids, $classes);
 
     }
 
-    private function check_name_exisist($name, $classes){
+    private function check_name_and_id_exisist($name, $classes){
         $new_name = $name;
         $i = 1;
         while (true) {
             $found = false;
             foreach ($classes as $index=>$class) {
-                if ($class['new_name'] == $new_name) {
+                if ($class['new_name'] === $new_name) {
                     $found = true;
                     $new_name = $name .$i;
                     $i++;
                     break;
                 }
             }
-            if (!$found) {
-                break;
-            }
+            if (!$found) break;
         }
         return $new_name;
     }
