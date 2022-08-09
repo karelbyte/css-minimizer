029bced692274754b87a363772f0ad2403bb2be3
Simon Montoya <simonmontoya19@gmail.com>
2022-08-08 20:22:42 -0500

    refactoring obtain class and ids and reemplace en html

--- app/Console/Commands/minimizercss.php
+++ app/Console/Commands/minimizercss.php
@@ -42,13 +42,27 @@ class minimizercss extends Command
         foreach ( $obtained_classes as $index =>$class) {
             if (strpos($class[1]," ")) {
                 $class_array = explode(" ", $class[1]);
-                foreach ($class_array as  $item) {
+                //dd($class_array);
+                foreach ($class_array as $key=>  $item) {
                     //echo 'class'.$item.'<br>';;
-                    $class = [
-                        0 => 'class='.$obtained_classes[$index][1],
-                        1 => $item,
-                        'original'  =>  'class='.$obtained_classes[$index][1]
-                    ];
+                    if ($key == 0) {
+                        $class = [
+                            0 => $obtained_classes[$index][1],
+                            1 => $item,
+                            'original'  =>  $obtained_classes[$index][1],
+                            'is_child' => false,
+                            'parent' =>'' ,
+                        ];
+                    } else {
+                        $class = [
+                            0 => $obtained_classes[$index][1],
+                            1 => $item,
+                            'original'  =>  $obtained_classes[$index][1],
+                            'is_child' => true,
+                            'parent' => $index,
+                        ];
+                    }
+
                     $class_name = $item;
                     $class['new_name'] = substr($class_name, 0, 1);
                     array_push($classes, $class);
@@ -57,14 +71,16 @@ class minimizercss extends Command
             }
             else{
                 $class_name = $class[1];
+                $class[0] = str_replace("class=", "", $class[0]);
                 $class['original'] = $class[1];
+                $class[ 'is_child'] = false;
+                 $class['parent' ] = '';
                 $class['new_name'] = substr($class_name, 0, 1);
 
                 array_push($classes, $class);
             }
 
         }
-        //dd($classes[0],$classes[1],$obtained_classes[0]);
 
         foreach ($obtained_ids as $index =>$id) {
             if (strpos($id[1]," ")) {
@@ -84,30 +100,30 @@ class minimizercss extends Command
                 $id['new_name'] = substr($id_name, 0, 1);
                 array_push($ids, $id);
             }
-           /* $id = $id[1];
-            $ids[$index]['new_name'] = substr($id, 0, 1);*/
         }
+        $html_modified='';
 
+        $classes = $this->unique_multidim_array($classes, 1);
+        $classes =$this->get_name_or_id( $classes);
+        //dd($classes);
         foreach ($classes as $index =>$class) {
+            $new_name = $class['new_name'];
 
-            $new_name = $this->check_name_and_id_exisist( $classes[$index]['new_name'], $classes);
-            $classes[$index]['new_name'] = $new_name;
-            $class_new_name = $classes[$index]['new_name'];
-            if ($index > 0 && $classes[$index][0] === $classes[$index]['original']) {
-                $classes[$index]['new_name'] = $classes[$index - 1]['new_name'] . ' ' . $class_new_name;
-                $original_element =  $html_string->find('.' . $classes[$index][1])[0];
-                if($original_element){
-                    $original_element->setAttribute('class', $class_new_name);
+            if ($index > 0) {
+
+                if ( $class[0] === $class['original'] && $class['is_child'] == true) {
+                    $html = preg_replace('/'.$class[0].'/', $class["parent"]." ".$new_name, $html);//str_ireplace($classes[$index][1], $new_name , $html);
                 }
-            }else{
-                $original_element =  $html_string->find('.' . $classes[$index][1])[0];
-                if($original_element){
-                    $original_element->setAttribute('class', $class_new_name);
+                else{
+                    $html = preg_replace('/'.$class[0].'/', $new_name, $html);
+
                 }
+
             }
 
         }
-        dd($classes[0],$classes[1] ,$dom->outerHtml);
+        dump($classes);
+        dd($html);
 
         foreach ($ids as $index =>$id) {
             $new_name = $this->check_name_exisist( $ids[$index]['new_name'], $ids);
@@ -123,22 +139,55 @@ class minimizercss extends Command
 
     }
 
-    private function check_name_and_id_exisist($name, $classes){
-        $new_name = $name;
-        $i = 1;
-        while (true) {
-            $found = false;
-            foreach ($classes as $index=>$class) {
-                if ($class['new_name'] === $new_name) {
-                    $found = true;
-                    $new_name = $name .$i;
-                    $i++;
-                    break;
-                }
+    protected function unique_multidim_array($array, $key) {
+        $temp_array = array();
+        $i = 0;
+        $key_array = array();
+
+        foreach($array as $val) {
+            if (!in_array($val[$key], $key_array)) {
+                $key_array[$i] = $val[$key];
+                $temp_array[$i] = $val;
             }
-            if (!$found) break;
+            $i++;
         }
-        return $new_name;
+
+        return $temp_array;
+    }
+
+    private function get_name_or_id( $classes) {
+        $classes =  array_values($classes);
+        $i = 0;
+
+        foreach ($classes as $index=>$class) {
+             $new_name = $class['new_name'] . $i;
+            $classes[$index]['new_name'] = $new_name;
+            if ($class['is_child'] == true) {
+                $parent = array_map(function ($item) use ($class) {
+                    if (substr($item['new_name'],0,1) == substr($class['new_name'],0,1)) {
+                        return $item['new_name'];
+                    }
+                    if ($item['original'] == $class['original'] && $item['is_child'] == false) {
+                        return $item['new_name'];
+                    }
+
+
+                }, $classes);
+                $parent = array_filter($parent);
+                $parent = array_values($parent);
+                if (count($parent) > 0) {
+                    //dump($parent);
+                    $classes[$index]['parent'] =$parent[0];
+                }
+
+
+            }
+
+
+             $i++;
+        }
+
+        return $classes;
     }
 
 
